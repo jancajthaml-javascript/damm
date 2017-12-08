aff581206a111ec4ad4c43744b2f224b858329c5
Jan Cajthaml <jan.cajthaml@gmail.com>
2017-12-08 22:12:59 +0100

    implemented

---
+++ tests/damm.test.js
@@ -0,0 +1,23 @@
+test("damm.Digit", async () => {
+  const damm = require("../src/damm.js")
+
+  expect(damm.Digit("0012301476470096832")).toBe(5)
+  expect(damm.Digit("xy-1")).toBe(-1)
+})
+
+test("damm.Validate", async () => {
+  const damm = require("../src/damm.js")
+
+  expect(damm.Validate("00123014764700968325")).toBe(true)
+  expect(damm.Validate("1234567812345678")).toBe(false)
+  expect(damm.Validate("xy-1")).toBe(false)
+})
+
+test("damm.Generate", async () => {
+  const damm = require("../src/damm.js")
+
+  let generated = damm.Generate("0012301476470096832")
+
+  expect(generated).toBe("00123014764700968325")
+  expect(damm.Validate(generated)).toBe(true)
+})
